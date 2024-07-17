4cd05829c0ec0c464ef17869f3fb484a33a63c2e
CJacobs <cjacobs3971@gmail.com>
2024-07-17 16:50:33 -0700

    env fixes

--- backend/app.py
+++ backend/app.py
@@ -178,6 +178,8 @@ def serve_react_app(path):
         return send_file(os.path.join(app.static_folder, 'index.html'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
