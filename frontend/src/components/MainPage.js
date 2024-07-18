4c775b430fa13bd066c583f96b59931446a3f673
CJacobs <cjacobs3971@gmail.com>
2024-07-18 11:24:47 -0700

    new fixes for profile pictures 2

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -152,7 +152,7 @@ const handleCommentSubmit = async (event, postId) => {
                 post.comments.map((comment) => (
                   <div key={comment.id} className="comment">
                     <div className="user_picture">
-                      <img src={`http://127.0.0.1:5000/profile_pics/${comment.user_profile_pic}`} alt="Profile" />
+                    <img src={comment.user_profile_pic} alt="Profile" />
                     </div>
                     <p>{comment.text}</p>
                   </div>
